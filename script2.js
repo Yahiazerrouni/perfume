b6cd1d792d4f89fcb38f915bdf930879d9345f0f
Yahiazerrouni <yahiazerrouni@gmail.com>
2025-05-09 21:01:35 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) { 
+    e.preventDefault();alert('تم إرسال رسالتك بنجاح!');
+});
+function addToCart(productName) { alert('تمت إضافة "' + productName + '" إلى سلة الشراء!'); }
